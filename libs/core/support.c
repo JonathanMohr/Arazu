aea41b202f94282d17949ac10eb4583cac0e1da9
Jonathan1324 <mohrjonathan09@gmail.com>
2026-06-20 10:43:20 +0200

    Added support.c

--- libs/core/support.c
+++ libs/core/support.c
@@ -1,6 +1,6 @@
 #include <arazu/core/types.h>
 
-void* memcpy(void* dst, const void* src, Arazu_Support_size_t n)
+void* memcpy(void* dst, const void* src, Arazu_Support_size_t n) // NOLINT(misc-use-internal-linkage)
 {
     Arazu_u8* d = dst;
     const Arazu_u8* s = src;
