62e49601e0fe69fa9f3156d7c15c9f5beb306e51
Jonathan <mohrjonathan09@gmail.com>
2026-06-23 15:35:10 +0200

    Added Arazu_Object

---
+++ include/arazu/core/object/object.h
@@ -0,0 +1,18 @@
+#ifndef ARAZU_CORE_OBJECT_OBJECT_H
+#define ARAZU_CORE_OBJECT_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../context.h"
+
+typedef struct Arazu_Object Arazu_Object;
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ libs/core/object/object.h
@@ -0,0 +1,21 @@
+#ifndef ARAZU_LCORE_OBJECT_OBJECT_H
+#define ARAZU_LCORE_OBJECT_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <arazu/core/object/object.h>
+
+struct Arazu_Object
+{
+    char tmp;
+};
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
