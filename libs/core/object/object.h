10ab7e3476688db699043129d27c97fa3dc9fd14
Jonathan <mohrjonathan09@gmail.com>
2026-06-29 16:26:23 +0200

    Added a few object stuff

--- libs/core/object/object.h
+++ libs/core/object/object.h
@@ -1,18 +1,47 @@
 #ifndef ARAZU_LCORE_OBJECT_OBJECT_H
 #define ARAZU_LCORE_OBJECT_OBJECT_H
 
-#include "arazu/core/types.h"
+#include "arazu/core/context.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include <arazu/core/object/object.h>
 
+typedef Arazu_u16 Arazu_Object_Relocation_Type;
+#define ARAZU_OBJECT_RELOCATION_TYPE_ABSOLUTE    ((Arazu_Object_Relocation_Type)0)
+#define ARAZU_OBJECT_RELOCATION_TYPE_PC_RELATIVE ((Arazu_Object_Relocation_Type)1)
+
+struct Arazu_Object_Relocation
+{
+    Arazu_u64 offsetInSection;
+    Arazu_i64 addend;
+
+    Arazu_String* symbol;
+
+    Arazu_u16 size;
+    Arazu_Object_Relocation_Type type;
+    Arazu_Bool isSigned;
+
+    Arazu_Bool littleEndian; // false -> big endian
+    Arazu_Bool isSection; // if symbol is a section
+};
+
 struct Arazu_Object_Symbol
 {
     Arazu_u64 value;
 
-    
+    Arazu_u64 line; // 0-based
+    Arazu_u64 column; // 0-based
+    Arazu_String file;
+
+    Arazu_String name;
+    Arazu_String sectionName;
+
+    Arazu_Bool hasDebugInformation; // use line, column and file only if this is true
+    Arazu_Bool inSection;
+    Arazu_Bool defined; // only used when isSection is false, if true, symbol is absolute, else, symbol is undefined
+    Arazu_Bool isGlobal;
 };
 
 typedef Arazu_u8 Arazu_Object_Section_Flags;
@@ -29,6 +58,9 @@ struct Arazu_Object_Section
     Arazu_u64 size;
     Arazu_u64 align;
 
+    struct Arazu_Object_Relocation* relocations;
+    struct Arazu_Object_Symbol* symbols; // symbols in section
+
     Arazu_u8* buffer;
     Arazu_String name;
 
@@ -38,7 +70,11 @@ struct Arazu_Object_Section
 
 struct Arazu_Object
 {
-    char tmp;
+    struct Arazu_Object_Section* sections;
+    struct Arazu_Object_Symbol* symbols; // absolute and undefined symbols
+
+    Arazu_String architecture; // just alphanumerical (all lowercase) + '_'
+    Arazu_String bitMode; // just alphanumerical (all lowercase) + '_'
 };
 
 
