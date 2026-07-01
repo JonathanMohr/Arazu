d00d199791bbb4afa4ea25f2221dee0aef5f14d1
Jonathan1324 <mohrjonathan09@gmail.com>
2026-07-01 10:36:34 +0200

    Formatted file correctly

--- libs/core/support.c
+++ libs/core/support.c
@@ -1,7 +1,8 @@
 #include <arazu/core/types.h>
 
 // NOLINTNEXTLINE(misc-use-internal-linkage)
-ARAZU_DETAIL_WEAK_FUNCTION(void*, memcpy, (void* dst, const void* src, Arazu_Support_size_t n), {
+ARAZU_DETAIL_WEAK_FUNCTION(void*, memcpy, (void* dst, const void* src, Arazu_Support_size_t n),
+{
     Arazu_u8* d = dst;
     const Arazu_u8* s = src;
     for (Arazu_Support_size_t i = 0; i < n; i++)
